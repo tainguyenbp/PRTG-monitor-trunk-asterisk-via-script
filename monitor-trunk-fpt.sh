c96926bde6ac1df647af23c1b5cd900c2279504f
tainguyenbp <nguyenngoctaibp@gmail.com>
2019-11-19 05:00:29 -0500

    update script monitor trunk

---
+++ monitor-trunk-fpt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+serviceIsRunning=false
+value_trunk_fpt=$(/usr/sbin/asterisk -rx "sip show peers" |grep SIP_TRUNK_FPT |awk '{print $6}'| sed 2,2d)
+
+if [ "$value_trunk_fpt" == OK ]
+    then
+        serviceIsRunning=true
+        echo "0:200:running"
+    fi
+if [ $serviceIsRunning == false ]
+    then
+        echo "2:404:UNREACHABLE"
+    fi
---
+++ monitor-trunk-vnpt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+serviceIsRunning=false
+value_trunk_vnpt=$(/usr/sbin/asterisk -rx "sip show peers" |grep SIP_TRUNK_VNPT |awk '{print $6}'| sed 2,2d)
+
+if [ "$value_trunk_vnpt" == OK ]
+    then
+        serviceIsRunning=true
+        echo "0:200:running"
+    fi
+if [ $serviceIsRunning == false ]
+    then
+        echo "2:404:UNREACHABLE"
+    fi
